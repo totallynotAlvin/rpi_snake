70fdb0dfd5557a76bed3d1350dfac1b5d93ef503
lomedi <102524169+lomedi@users.noreply.github.com>
2022-04-04 20:12:23 +0800

    Update food.py

--- model/food.py
+++ model/food.py
@@ -6,5 +6,12 @@ class Food:
 
 	def get_random_position(self):
 		pass
-	
-	
+
+list_of_coordinates = [1,2,3,4,5,6,7,8]
+
+Food_x = sample(list_of_coordinates, 1)
+Food_y = sample(list_of_coordinates, 1)
+
+coordinates = (Food_x, Food_y)
+
+food = Food((1,1), coordinates)
