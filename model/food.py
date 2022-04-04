333e8b096990de9c8933cae6b155933236b7292a
lomedi <102524169+lomedi@users.noreply.github.com>
2022-04-04 21:35:51 +0800

    Update food.py

--- model/food.py
+++ model/food.py
@@ -1,5 +1,5 @@
 from util import Point
-import random
+from random import randomint
 
 class Food:
 	def __init__(self, width=8, height=8):
@@ -11,6 +11,15 @@ class Food:
 	
 		self.position: coordinates
 
+		#OR
+		
+		self.position: randint(0,8), randint(0,8)
+			
+	def eat(self, condition, disappearance)
+	
+		If snake[0] == food
+			food = ()
+			
 	def get_random_position(self, snake_pos: list[Point]):
 		possible_points = [Point(0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (0,6), (0,7),
 				  (1,0), (1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7),
@@ -20,6 +29,5 @@ class Food:
 				  (5,0), (5,1), (5,2), (5,3), (5,4), (5,5), (5,6), (5,7),
 				  (6,0), (6,1), (6,2), (6,3), (6,4), (6,5), (6,6), (6,7),
 				  (7,0), (7,1), (7,2), (7,3), (7,4), (7,5), (7,6), (7,7),] - snake_pos
-		random(possible_points)
+		sample(possible_points, 1)
 		pass
-
