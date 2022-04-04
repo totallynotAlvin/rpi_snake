3db142df141cf962b58f20427676209ac311be69
Alvin Shrestha <alvin@Alvins-MacBook-Pro.local>
2022-04-04 20:21:31 +0800

    extra details on food

--- model/food.py
+++ model/food.py
@@ -1,11 +1,15 @@
 from util import Point
+import random
 
 class Food:
-	def __init__(self):
+	def __init__(self, width=8, height=8):
 		self.position: Point
 
-	def get_random_position(self):
+	def get_random_position(self, snake_pos: list[Point]):
+		possible_points = [Point(0, 0), ]
 		pass
+
+
 
 list_of_coordinates = [1,2,3,4,5,6,7,8]
 
